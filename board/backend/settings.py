6cb234334ca6e626fd25ddab1d76ff14b438e4d8
KakaMbweha01 <leonestill804@gmail.com>
2025-01-30 16:44:27 +0300

    added user authentication

--- board/backend/settings.py
+++ board/backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'notices',
+    'rest_framework_simplejwt',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK ={
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+#AUTH_USER_MODEL = 'accounts.CustomerUser'
--- board/notices/views.py
+++ board/notices/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.permissions import BasePermission
 from .models import Notice
 from .serializers import NoticeSerializer
 
 # Create your views here.
 
+# role-based permissions(admin only can create notices)
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'admin'
+
 # list all notices, create a new notice
 class NoticeListCreateView(generics.ListCreateAPIView):
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.username)
 
 # retrieve, update or delete a notice
 class NoticeDetailView(generics.RetrieveUpdateDestroyAPIView):
