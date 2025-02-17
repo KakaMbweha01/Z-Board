ab9a0ac4785970ae50c9abac62bcac1c5ddaf8f9
KakaMbweha01 <leonestill804@gmail.com>
2025-02-17 17:01:08 +0300

    trying notifs

--- noticeboard/notifications/views.py
+++ noticeboard/notifications/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Notification
 
 # Create your views here.
+
+# notification view
+def notification_list(request):
+    notifications = Notification.objects.all().order_by('-created_at')
+    return render(request, 'notifications/list.html', {'notifications': notifications})
