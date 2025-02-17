f763a183a25e213def8997474eb5dfcd175b1ccc
KakaMbweha01 <leonestill804@gmail.com>
2025-02-17 15:50:52 +0300

    that was trash lets redo it

--- board/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, CustomTokenObtainPairView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-]
--- board/notices/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Notice, Category
-
-# Register your models here.
-
-admin.site.register(Notice)
-admin.site.register(Category)
